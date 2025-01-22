e271a054e4a94438fbd39200b817e599f9f8f89c
XPosterMaloneX <47110015+XPosterMaloneX@users.noreply.github.com>
2025-01-22 18:31:58 -0500

    Added first name, last name, and email fields to registration page.
    Added the ability for the user to enter first and last name, and add their email on the registration page, the code checks to see if the email has been registered already as well as already checking for the username.

--- src/Register/register.php
+++ src/Register/register.php
@@ -1,4 +1,7 @@
 <?php
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
 $username = $_POST['username'];
 $password = $_POST['password'];
 $confirm_password = $_POST['confirm_password'];
@@ -22,15 +25,15 @@ $con = new mysqli("localhost", "root", "Tsj123456+", "4p02_group_login_db");
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 } else {
-    $stmt = $con->prepare("SELECT * FROM login WHERE username = ?");
-    $stmt->bind_param("s", $username);
+    $stmt = $con->prepare("SELECT * FROM login WHERE username = ? OR email = ?");
+    $stmt->bind_param("ss", $username, $email);
     $stmt->execute();
     $stmt_result = $stmt->get_result();
     if ($stmt_result->num_rows > 0) {
-        echo "Username already exists.";
+        echo "Username or email already exists.";
     } else {
-        $stmt = $con->prepare("INSERT INTO login (username, password) VALUES (?, ?)");
-        $stmt->bind_param("ss", $username, $password);
+        $stmt = $con->prepare("INSERT INTO login (first_name, last_name, email, username, password) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $first_name, $lastn_name, $email, $username, $password);
         if ($stmt->execute()) {
             header("Location: ../Login/index.html");
             echo "Registration successful.";
